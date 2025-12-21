60be2bd02a2793e10358fd3c87a2431540c377ff
Lucien Thomas <lucienthomas00@gmail.com>
2025-12-21 02:11:06 -0600

    fix

--- src/pipelines/text_generation/pipeline.rs
+++ src/pipelines/text_generation/pipeline.rs
@@ -122,7 +122,6 @@ impl<M: TextGenerationModel + Send> TextGenerationPipeline<M> {
         }
         Ok(outputs)
     }
-
     async fn prompt_completion_internal(&self, prompt: &str) -> anyhow::Result<String> {
         let (result, _) = self.prompt_completion_internal_with_stats(prompt).await?;
         Ok(result)
